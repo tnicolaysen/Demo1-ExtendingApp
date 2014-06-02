38889e132f9bebe08c090227552280dde69765ef
Torstein Nicolaysen <tnicolaysen@gmail.com>
2014-06-02 23:02:14 +0200

    Completed logging for ElasticSearch

---
+++ GenericWebApp/Logging/ElasticSearchTarget.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NLog.Common;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+
+namespace GenericWebApp.Logging
+{
+    [Target("ElasticSearch")]
+    public sealed class ElasticSearchTarget : TargetWithLayout
+    {
+        [RequiredParameter]
+        public Layout Url { get; set; }
+
+        public ElasticSearchTarget()
+        {
+            // Harcdoded. Won't work if it doesn't render proper JSON.
+            Layout = Layout.FromString("$(jsondump)");
+        }
+
+        protected override void Write(AsyncLogEventInfo info)
+        {
+            try
+            {
+                SendToElasticSearch(info);
+            }
+            catch (Exception ex)
+            {
+                info.Continuation(ex);
+            }
+        }
+
+        private void SendToElasticSearch(AsyncLogEventInfo info)
+        {
+            var json = Layout.Render(info.LogEvent);
+            var uri = GetUriFromLayout(info);
+
+             var jsonClient = new SimpleElasticSearchWebClient();
+            jsonClient.Error += exception => info.Continuation(exception);
+            jsonClient.PostAsync(uri, json);
+        }
+
+        private Uri GetUriFromLayout(AsyncLogEventInfo info)
+        {
+            return new Uri(Url.Render(info.LogEvent));
+        }
+    }
+}
---
+++ GenericWebApp/Logging/JsonDumpRenderer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using Newtonsoft.Json;
+using NLog;
+using NLog.LayoutRenderers;
+
+namespace GenericWebApp.Logging
+{
+    [LayoutRenderer("jsondump")]
+    public class JsonDumpLayoutRenderer : LayoutRenderer
+    {
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var logEntry = CreateLogEntry(logEvent);
+            builder.Append(logEntry);
+        }
+
+        private string CreateLogEntry(LogEventInfo @event)
+        {
+            // Dynamic FTW! Easy to add properties.
+            dynamic logEntry = new ExpandoObject();
+            logEntry.Level = @event.Level.Name;
+            logEntry.LoggerName = @event.LoggerName;
+
+            // Slight hack to add the special timestamp property
+            ((IDictionary<String, Object>) logEntry).Add("@timestamp", @event.TimeStamp.ToUniversalTime().ToString("O"));
+
+            // Using NLog's built in "renderers" to get more details
+            logEntry.Message = RenderUsing<MessageLayoutRenderer>(@event);
+            logEntry.Identity = RenderUsing<IdentityLayoutRenderer>(@event);
+            logEntry.MachineName = RenderUsing<MachineNameLayoutRenderer>(@event);
+            logEntry.WindowsIdentity = RenderUsing<WindowsIdentityLayoutRenderer>(@event);
+            logEntry.ThreadId = RenderUsing<ThreadIdLayoutRenderer>(@event);
+            logEntry.ProcessId = RenderUsing<ProcessIdLayoutRenderer>(@event);
+
+            logEntry.NDC = "2014";
+            // Add your own relevant information here
+
+            if (@event.Exception != null)
+                logEntry.ErrorDetails = CreateExceptionEntry(@event.Exception);
+
+            return JsonConvert.SerializeObject(logEntry, Formatting.Indented);
+        }
+
+        #region Magic little helpers
+
+        private static string RenderUsing<TRenderer>(LogEventInfo @event)
+            where TRenderer : LayoutRenderer, new()
+        {
+            return new TRenderer().Render(@event);
+        }
+
+        private static dynamic CreateExceptionEntry(Exception ex)
+        {
+            // Hint: Make it recursive if you want to get inner exceptions
+            dynamic exception = new ExpandoObject();
+            exception.Type = ex.GetType().ToString();
+            exception.Message = ex.Message;
+            exception.ExceptionData = ex.Data;
+            exception.StackTraceString = ex.StackTrace;
+            return exception;
+        }
+
+        #endregion
+    }
+}   
